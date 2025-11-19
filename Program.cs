5a3f0e3ccce2f046a5805cb072c52ddf686155e9
Cristian <cristianrodrigues93@gmail.com>
2025-11-18 21:06:18 -0300

    Ajuste de CORS para branch DEV

--- Program.cs
+++ Program.cs
@@ -127,7 +127,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("dev", policy =>
-        policy.WithOrigins("https://dev-barbearia-gabriel-port.vercel.app/")
+        policy.WithOrigins("https://dev-barbearia-gabriel-port.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials());
